5bde79fb817ff3367a15ee9943ae9ca47a6539ad
Luca Belardinelli <belardinelliluca@gmail.com>
2025-03-14 17:07:18 +0100

    file multer

---
+++ middlewares/multer.js
@@ -0,0 +1,18 @@
+// per la gestione dell'upload di file
+const multer = require("multer");
+
+// File upload middleware
+const storage = multer.diskStorage({
+    //cartella in cui verranno salvati i file
+    destination: "./public/img/books/",
+    filename: (req, file, cb) => {
+        // Crea un nome univoco usando il timestamp
+        const uniqueName = `${Date.now()}-${file.originalname}`;
+        // Salva il file con il nuovo nome
+        cb(null, uniqueName);
+    },
+});
+// Crea l'istanza di Multer con lo storage configurato
+const upload = multer({ storage });
+
+module.exports = upload;
